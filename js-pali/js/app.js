ab71860c8368d21baaf57e5a2f7aefce573947d4
Clipper4rancione <meluziis.ciro99@gmail.com>
2022-10-19 16:20:19 +0200

    first commit and push added input in index/js-pali

---
+++ js-pali/js/app.js
@@ -0,0 +1,7 @@
+/*
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+//INPUT DELLA PAROLA
+let word = document.querySelector('input').value;
